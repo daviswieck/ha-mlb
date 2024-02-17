749d4a15f7a3a38bb6c4c3ed4496e73640c77b6e
gopher_dave <79770840+daviswieck@users.noreply.github.com>
2024-02-16 22:09:04 -0600

    Update const.py

--- custom_components/college_football/const.py
+++ custom_components/college_football/const.py
@@ -1,5 +1,5 @@
 # API
-API_ENDPOINT = "https://site.api.espn.com/apis/site/v2/sports/football/college-football/scoreboard?groups=80&limit=150" 
+API_ENDPOINT = "https://site.api.espn.com/apis/site/v2/sports/baseball/college-baseball/scoreboard" 
 USER_AGENT = "Mozilla/5.0 (Macintosh; Intel Mac OS X 11_6) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/15.0 Safari/605.1.15"
 
 # Config
@@ -7,14 +7,14 @@ CONF_TIMEOUT = "timeout"
 CONF_TEAM_ID = "team_id"
 
 # Defaults
-DEFAULT_ICON = "mdi:football"
-DEFAULT_NAME = "College_Football"
+DEFAULT_ICON = "mdi:baseball"
+DEFAULT_NAME = "College_Baseball"
 DEFAULT_TIMEOUT = 240
 
 # Misc
 TEAM_ID = ""
 VERSION = "0.1"
-ISSUE_URL = "https://github.com/daviswieck/ha-college-football"
+ISSUE_URL = "https://github.com/daviswieck/ha-college-baseball"
 DOMAIN = "college_football"
 PLATFORM = "sensor"
 ATTRIBUTION = "Data provided by ESPN"
