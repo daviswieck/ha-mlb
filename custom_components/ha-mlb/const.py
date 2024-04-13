6c905f50c677b8544bbfa37343536231b3e7687b
gopher_dave <79770840+daviswieck@users.noreply.github.com>
2024-04-13 09:07:35 -0500

    Update const.py

--- custom_components/ha-mlb/const.py
+++ custom_components/ha-mlb/const.py
@@ -1,5 +1,5 @@
 # API
-API_ENDPOINT = "https://site.api.espn.com/apis/site/v2/sports/baseball/college-baseball/scoreboard" 
+API_ENDPOINT = "https://site.api.espn.com/apis/site/v2/sports/baseball/mlb/scoreboard" 
 USER_AGENT = "Mozilla/5.0 (Macintosh; Intel Mac OS X 11_6) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/15.0 Safari/605.1.15"
 
 # Config
@@ -14,7 +14,7 @@ DEFAULT_TIMEOUT = 240
 # Misc
 TEAM_ID = ""
 VERSION = "0.1"
-ISSUE_URL = "https://github.com/daviswieck/ha-college-baseball"
+ISSUE_URL = "https://github.com/daviswieck/ha-mlb"
 DOMAIN = "college_football"
 PLATFORM = "sensor"
 ATTRIBUTION = "Data provided by ESPN"
