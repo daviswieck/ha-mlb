06445441dd94b8622df1b1a119551173ebe4b2a7
gopher_dave <79770840+daviswieck@users.noreply.github.com>
2025-06-26 17:12:06 -0500

    Update const.py

--- custom_components/ha-mlb/const.py
+++ custom_components/ha-mlb/const.py
@@ -1,22 +1,25 @@
 # API
-API_ENDPOINT = "https://site.api.espn.com/apis/site/v2/sports/baseball/mlb/scoreboard" 
-USER_AGENT = "Mozilla/5.0 (Macintosh; Intel Mac OS X 11_6) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/15.0 Safari/605.1.15"
+API_ENDPOINT = "https://site.api.espn.com/apis/site/v2/sports/baseball/mlb/scoreboard"
+USER_AGENT = (
+    "Mozilla/5.0 (Macintosh; Intel Mac OS X 11_6) "
+    "AppleWebKit/605.1.15 (KHTML, like Gecko) Version/15.0 Safari/605.1.15"
+)
 
-# Config
+# Config keys
 CONF_TIMEOUT = "timeout"
 CONF_TEAM_ID = "team_id"
 
 # Defaults
 DEFAULT_ICON = "mdi:baseball"
 DEFAULT_NAME = "MLB"
-DEFAULT_TIMEOUT = 240
+DEFAULT_TIMEOUT = 240  # seconds
 
-# Misc
-TEAM_ID = ""
-VERSION = "0.1"
-ISSUE_URL = "https://github.com/daviswieck/ha-mlb"
+# Miscellaneous constants
 DOMAIN = "ha-mlb"
 PLATFORM = "sensor"
-ATTRIBUTION = "Data provided by ESPN"
+PLATFORMS = [PLATFORM]
 COORDINATOR = "coordinator"
-PLATFORMS = ["sensor"]
+ATTRIBUTION = "Data provided by ESPN"
+VERSION = "0.1"
+ISSUE_URL = "https://github.com/daviswieck/ha-mlb"
+TEAM_ID = ""  # placeholder or default, probably unused
