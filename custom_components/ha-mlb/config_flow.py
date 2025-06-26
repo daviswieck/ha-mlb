9b577b5d61ef9d6b0dd9ebd609284b046e221a75
gopher_dave <79770840+daviswieck@users.noreply.github.com>
2025-06-26 17:05:39 -0500

    Update config_flow.py

--- custom_components/ha-mlb/config_flow.py
+++ custom_components/ha-mlb/config_flow.py
@@ -21,14 +21,21 @@ from .const import (
     USER_AGENT,
 )
 
-JSON_FEATURES = "features"
-JSON_PROPERTIES = "properties"
-JSON_ID = "id"
-
 _LOGGER = logging.getLogger(__name__)
 
 
-def _get_schema(    
+def _get_team_list() -> list:
+    """Return list of team acronyms"""
+    team_list = [
+        "HOU", "TEX", "NYY", "BOS", "LAD", "SF", "CHC", "STL", "ATL", "NYM",
+        "SEA", "OAK", "CLE", "CWS", "MIL", "MIN", "TB", "TOR", "PHI", "WSH",
+        "ARI", "SD", "COL", "MIA", "CIN", "PIT", "DET", "KC", "BAL", "LAA"
+    ]
+    _LOGGER.debug("Team list: %s", team_list)
+    return team_list
+
+
+def _get_schema(
     hass: HomeAssistant,
     user_input: Optional[Dict[str, Any]],
     default_dict: Dict[str, Any],
@@ -38,30 +45,16 @@ def _get_schema(
     if user_input is None:
         user_input = {}
 
-    def _get_default(key: str, fallback_default: Any = None) -> None:
-        """Gets default value for key."""
+    def _get_default(key: str, fallback_default: Any = None) -> Any:
         return user_input.get(key, default_dict.get(key, fallback_default))
 
     return vol.Schema(
         {
-            vol.Required(CONF_TEAM_ID, default=_get_default(CONF_TEAM_ID,_get_team_list())): str,
+            vol.Required(CONF_TEAM_ID, default=_get_default(CONF_TEAM_ID, "HOU")): str,
             vol.Optional(CONF_NAME, default=_get_default(CONF_NAME, DEFAULT_NAME)): str,
             vol.Optional(CONF_TIMEOUT, default=_get_default(CONF_TIMEOUT, DEFAULT_TIMEOUT)): int,
         }
     )
-
-
-def _get_team_list() -> list:
-    """Return list of team acronyms"""
-
-    team_list = [
-    "HOU", "TEX", "NYY", "BOS", "LAD", "SF", "CHC", "STL", "ATL", "NYM",
-    "SEA", "OAK", "CLE", "CWS", "MIL", "MIN", "TB", "TOR", "PHI", "WSH",
-    "ARI", "SD", "COL", "MIA", "CIN", "PIT", "DET", "KC", "BAL", "LAA"
-]
-
-    _LOGGER.debug("Team list: %s", team_list)
-    return team_list
 
 
 @config_entries.HANDLERS.register(DOMAIN)
@@ -76,33 +69,27 @@ class MLBBaseballScoresFlowHandler(config_entries.ConfigFlow, domain=DOMAIN):
         self._data = {}
         self._errors = {}
 
-    # async def async_step_import(self, user_input: dict[str, Any]) -> FlowResult:
-    #     """Import a config entry."""
-
-    #     user_input = user_input[DOMAIN]
-    #     result: FlowResult = await self.async_step_user(user_input=user_input)
-    #     if errors := result.get("errors"):
-    #         return self.async_abort(reason=next(iter(errors.values())))
-    #     return result
-
-    async def async_step_user(self, user_input={}):
+    async def async_step_user(self, user_input=None) -> FlowResult:
         """Handle a flow initialized by the user."""
+        if user_input is None:
+            user_input = {}
+
         self._errors = {}
         self._team_list = _get_team_list()
 
-        if user_input is not None:
+        if user_input:
             self._data.update(user_input)
-            return self.async_create_entry(title=self._data[CONF_NAME], data=self._data)
+            return self.async_create_entry(title=self._data.get(CONF_NAME, DEFAULT_NAME), data=self._data)
+
         return await self._show_config_form(user_input)
 
     async def _show_config_form(self, user_input):
         """Show the configuration form to edit location data."""
 
-        # Defaults
         defaults = {
             CONF_NAME: DEFAULT_NAME,
             CONF_TIMEOUT: DEFAULT_TIMEOUT,
-            CONF_TEAM_ID: self._team_list,
+            CONF_TEAM_ID: "HOU",
         }
 
         return self.async_show_form(
